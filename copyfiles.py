e91be46767d3521b7a7d8d7fbebd7a38655c88f1
leafcoder <leafcoder@gmail.com>
2016-05-06 15:18:54 +0800

    Create copyfiles.py

---
+++ copyfiles.py
@@ -0,0 +1,21 @@
+import os
+import shutil
+from posixpath import join as posix_join, exists as posix_exists, \
+  relpath as posix_relpath, normpath as posix_normpath
+
+def copyfiles(dirsrc, dirdst):
+    for dirpath, dirnames, filenames in os.walk(dirsrc):
+        relpath = posix_relpath(dirpath, dirsrc)
+        for dirname in dirnames:
+            srcpath = posix_normpath(posix_join(dirpath, dirname))
+            dstpath = posix_normpath(posix_join(dirdst, relpath, dirname))
+            if not posix_exists(dstpath):
+                os.makedirs(dstpath)
+        for filename in filenames:
+            srcpath = posix_normpath(posix_join(dirpath, filename))
+            dstpath = posix_normpath(posix_join(dirdst, relpath, filename))
+            if not posix_exists(dstpath):
+                shutil.copy(srcpath, dstpath)
+                
+if '__main__' == __name__:
+  copyfiles('./dir0', './dir1')
