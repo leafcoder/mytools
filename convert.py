418071477b1709a93ccfb2277ce40ab658dcda55
leafcoder <leafcoder@gmail.com>
2016-05-30 09:44:30 +0800

    Create convert.py

---
+++ convert.py
@@ -0,0 +1,10 @@
+import math
+
+def convert_filesize(size):
+   if (size == 0):
+       return '0B'
+   size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+   i = int(math.floor(math.log(size,1024)))
+   p = math.pow(1024,i)
+   s = round(size/p,2)
+   return '%s %s' % (s,size_name[i])
